29ed41cfb41248547d88b08cfdec7c949fd3200e
7imbrook <timbrook480@gmail.com>
2014-04-13 17:06:21 -0400

    Initial Commit

---
+++ intro app/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  intro app
+//
+//  Created by Michael Timbrook on 4/13/14.
+//  Copyright (c) 2014 Michael Timbrook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
