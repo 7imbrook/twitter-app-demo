75bc48ec822afe7a0b71329a219b17a50cb36e80
7imbrook <timbrook480@gmail.com>
2014-04-14 15:23:18 -0400

    Its a twitter app

--- intro app/TimelineController.m
+++ intro app/TimelineController.m
@@ -6,6 +6,9 @@
 //  Copyright (c) 2014 Michael Timbrook. All rights reserved.
 //
 
+@import Social;
+@import Accounts;
+
 #import "TimelineController.h"
 #import "TweetCell.h"
 
@@ -17,15 +20,8 @@
 {
     @private
     NSArray *tweets;
-}
-
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
+    ACAccount *account;
+    NSOperationQueue *imageLoading;
 }
 
 - (void)viewDidLoad
@@ -33,6 +29,35 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     self.collectionView.dataSource = self;
+
+    imageLoading = [NSOperationQueue new];
+
+    // Get Twitter Account
+    ACAccountStore *store = [ACAccountStore new];
+    ACAccountType *typeTwitter = [store accountTypeWithAccountTypeIdentifier:ACAccountTypeIdentifierTwitter];
+    [store requestAccessToAccountsWithType:typeTwitter options:nil completion:^(BOOL granted, NSError *error) {
+        if (granted) {
+            NSArray *accounts = [store accountsWithAccountType:typeTwitter];
+            if (accounts.count > 0){
+                account = accounts[0];
+                NSURL *url = [NSURL URLWithString:@"https://api.twitter.com/1.1/statuses/home_timeline.json"];
+                SLRequest *timeline = [SLRequest requestForServiceType:SLServiceTypeTwitter
+                                                         requestMethod:SLRequestMethodGET
+                                                                   URL:url
+                                                            parameters:nil];
+                [timeline setAccount:account];
+                [timeline performRequestWithHandler:^(NSData *responseData, NSHTTPURLResponse *urlResponse, NSError *error) {
+                    tweets = [NSJSONSerialization JSONObjectWithData:responseData options:NSJSONReadingMutableContainers error:nil];
+                    dispatch_async(dispatch_get_main_queue(), ^{
+                        [self.collectionView reloadData];
+                    });
+                }];
+
+            }
+        } else {
+            // Fail Case
+        }
+    }];
 }
 
 - (void)didReceiveMemoryWarning
@@ -41,13 +66,28 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)loadImage:(NSURL *)url toImageView:(UIImageView *)imageView
+{
+    NSURLRequest *request = [NSURLRequest requestWithURL:url.absoluteURL];
+    [NSURLConnection sendAsynchronousRequest:request queue:imageLoading completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            imageView.image = [UIImage imageWithData:data];
+        });
+    }];
+}
+
 #pragma mark - Datasource
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
 {
     TweetCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"tweet" forIndexPath:indexPath];
+    NSDictionary *attr = tweets[indexPath.row];
 
-    // Configure cell here
+    NSURL *profileURL = [NSURL URLWithString:attr[@"user"][@"profile_image_url"]];
+    NSLog(@"%@", profileURL);
+
+    cell.tweet.text = attr[@"text"];
+    [self loadImage:profileURL toImageView:cell.profileImage];
 
     return cell;
 }
